41554ed4ca18193d40a551905e746a77f030db02
guoqianqian <872122441@qq.com>
2022-05-20 16:47:54 +0800

    init

---
+++ QNLiveKitDemo/QNLiveKitDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  QNLiveKitDemo
+//
+//  Created by 郭茜 on 2022/5/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QNLiveKitDemo/QNLiveKitDemo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  QNLiveKitDemo
+//
+//  Created by 郭茜 on 2022/5/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
