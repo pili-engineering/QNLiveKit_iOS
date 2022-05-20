8e5458d031bb83c6605b9eb8c8a53b32196b4b9e
guoqianqian <872122441@qq.com>
2022-05-20 16:47:54 +0800

    init

---
+++ QNLiveKitDemo/QNLiveKitDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  QNLiveKitDemo
+//
+//  Created by 郭茜 on 2022/5/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ QNLiveKitDemo/QNLiveKitDemo/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  QNLiveKitDemo
+//
+//  Created by 郭茜 on 2022/5/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
