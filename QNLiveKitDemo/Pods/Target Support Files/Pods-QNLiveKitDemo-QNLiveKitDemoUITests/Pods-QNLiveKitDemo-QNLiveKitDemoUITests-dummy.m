722b22a5e746322bc0c8488ac053e5237df0d089
guoqianqian <872122441@qq.com>
2022-05-25 16:15:41 +0800

    gitnore

--- QNLiveKitDemo/Pods/Target Support Files/Pods-QNLiveKitDemo-QNLiveKitDemoUITests/Pods-QNLiveKitDemo-QNLiveKitDemoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_QNLiveKitDemo_QNLiveKitDemoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_QNLiveKitDemo_QNLiveKitDemoUITests
-@end
--- QNLiveKitDemo/Pods/Target Support Files/Pods-QNLiveKitDemoTests/Pods-QNLiveKitDemoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_QNLiveKitDemoTests : NSObject
-@end
-@implementation PodsDummy_Pods_QNLiveKitDemoTests
-@end
