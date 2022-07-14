4430599c7c673e623a9cb0d5232616f91b55fd01
guoqianqian <872122441@qq.com>
2022-07-14 15:52:26 +0800

    小黄车

---
+++ QNLiveKit/QNLiveKit/LiveUIKit/VC/GoodsListController.h
@@ -0,0 +1,19 @@
+//
+//  GoodsListController.h
+//  QNLiveKit
+//
+//  Created by 郭茜 on 2022/7/13.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+//商品列表页面
+@interface GoodsListController : UIViewController
+
+- (instancetype)initWithLiveID:(NSString *)liveID;
+
+@end
+
+NS_ASSUME_NONNULL_END
