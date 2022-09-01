71b05b343a893262cc6794e94fbf22108a29f3b7
guoqianqian <872122441@qq.com>
2022-09-01 11:10:22 +0800

    update SDK

--- DownloadResource/QNLiveKitDemo/QNLiveKitDemo/QNLiveKit.framework/Headers/QLiveListController.h
+++ DownloadResource/QNLiveKitDemo/QNLiveKitDemo/QNLiveKit.framework/Headers/QLiveListController.h
@@ -15,9 +15,9 @@ NS_ASSUME_NONNULL_BEGIN
 //创建房间被点击回调（需要自己实现跳转时实现）
 @property (nonatomic, copy) void (^createRoomClickedBlock)(void);
 //主播进房回调（需要自己实现跳转时实现）
-@property (nonatomic, copy) void (^masterJoinBlock)(QNLiveRoomInfo *itemModel);
+@property (nonatomic, copy) void (^masterJoinBlock)(QNLiveRoomInfo *roomInfo);
 //观众进房回调（需要自己实现跳转时实现）
-@property (nonatomic, copy) void (^audienceJoinBlock)(QNLiveRoomInfo *itemModel);
+@property (nonatomic, copy) void (^audienceJoinBlock)(QNLiveRoomInfo *roomInfo);
 
 @end
 
